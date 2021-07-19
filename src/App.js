37fedec8d0ad96232d3b67248d6af65c30d73e95
pdcarter1 <pdcarter1@FASTHOME>
2021-07-20 06:17:48 +1200

    ticket landing page added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { DefaultLayout } from './loayout/DefaultLayout';
 //import { Login } from './pages/login/login.page';
 //import {AddTicket} from './pages/new-ticket/AddTicket.page';
 import { TicketLists } from './pages/ticket-list/TicketLists.page';
+import { Ticket } from './pages/ticket/Ticket.page';
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       <DefaultLayout>
         {/*<Dashboard />*/}
         {/*<AddTicket />*/}
-        <TicketLists />
+        {/*<TicketLists />*/}
+        <Ticket />
       </DefaultLayout>    
     </div>
   );
